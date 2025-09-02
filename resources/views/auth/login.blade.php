ecbae8cc2836470646d8d0a28935d030b6193871
ReXooGen <ajulian.fernando123@gmail.com>
2025-09-02 17:35:54 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Presensi PKL - Login</title>
-    <link rel="icon" type="image/png" href="images/logo-smkn1-sby.png">
+    <link rel="icon" type="image/png" href="images/smk-negeri-1sby.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light d-flex align-items-center" style="min-height:100vh;">
