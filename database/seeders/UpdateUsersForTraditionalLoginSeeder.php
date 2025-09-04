c01f28e785e101868f442d2b6c32c802904876e6
ReXooGen <ajulian.fernando123@gmail.com>
2025-09-04 07:14:59 +0700

    update

--- database/seeders/UpdateUsersForTraditionalLoginSeeder.php
+++ database/seeders/UpdateUsersForTraditionalLoginSeeder.php
@@ -36,23 +36,28 @@ class UpdateUsersForTraditionalLoginSeeder extends Seeder
             ]
         );
 
-        // Update all existing users who don't have passwords with default password
-    $usersWithoutPassword = User::whereNull('password')->orWhere('password', '')->get();
-        
-        foreach ($usersWithoutPassword as $user) {
-            $user->update([
-        'password' => Hash::make('defaultpass123'), // Default password for existing users
-        'temp_password' => 'defaultpass123',
-        'force_password_change' => true,
-                'email_verified_at' => now(),
-            ]);
-            
-            $this->command->info("Updated user: {$user->email} with default password 'defaultpass123'");
+        // Update all existing users who need a password (no password OR no temp_password flag set)
+        $usersNeedingPassword = User::where(function($q){
+                $q->whereNull('password')->orWhere('password','');
+            })
+            ->orWhereNull('temp_password')
+            ->where('is_admin', false)
+            ->get();
+
+        foreach ($usersNeedingPassword as $user) {
+            $user->forceFill([
+                'password' => Hash::make('defaultpass123'),
+                'temp_password' => 'defaultpass123',
+                'force_password_change' => true,
+                'email_verified_at' => $user->email_verified_at ?? now(),
+            ])->save();
+
+            $this->command->info("Reset user: {$user->email} -> defaultpass123");
         }
 
         $this->command->info('Users updated successfully for traditional login system!');
         $this->command->info('Admin: admin@smkn1sby.sch.id / Surabaya99');
         $this->command->info('Testing: testing@smkn1sby.sch.id / testing123');
-    $this->command->info('Other users: [their_email] / defaultpass123 (WAJIB ganti password saat login)');
+        $this->command->info('Other users: [their_email] / defaultpass123 (WAJIB ganti password saat login)');
     }
 }
